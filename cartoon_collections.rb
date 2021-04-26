90ec909c9bb4adc8e5ca9d5f875c8c32dafbd753
Patrick Lynch <pajlynch@gmail.com>
2021-04-26 19:50:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,3 +5,9 @@ def roll_call_dwarves(dwarves)
     puts "#{index}. #{name}"
   end
 end
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    call.capitalize << "!"
+  end
+end
