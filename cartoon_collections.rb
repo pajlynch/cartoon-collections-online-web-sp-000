9fdf4b0687d285b999ace0a17443187de7cb1e85
Patrick Lynch <pajlynch@gmail.com>
2021-04-26 19:44:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,24 +5,3 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.collect do |call|
-    call.capitalize << "!"
-  end
-end
-
-def long_planeteer_calls(calls)
-  calls.any? do |call|
-    call.length > 4
-  end
-end
-
-def find_the_cheese(array)
-  # the array below is here to help
-  i = 0
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  while i < cheese_types.length
-    return cheese_types[i] if array.include?(cheese_types[i])
-    i += 1
-  end
-end
