59a0d70db26f6f10d76189b2f54aa0506743d1df
Patrick Lynch <pajlynch@gmail.com>
2021-04-26 19:50:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves = ["Dopey", "Bashful", "Doc", "Grumpy", "Happy", "Sleepy", "Sneezy"]
+  dwarves = ["Dopey", "Grumpy", "Bashful", "Doc", "Happy", "Sleepy", "Sneezy"]
   dwarves.each_with_index do |name, index|
     index += 1
     puts "#{index}. #{name}"
